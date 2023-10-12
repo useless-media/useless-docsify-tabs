8ff86b1c1c967cbe114a9f51ce7c4da35aedd264
Hyddan <daniel.hedenius@gmail.com>
2023-10-12 11:44:17 +0200

    useless docsify tabs

---
+++ ReadMe.md
@@ -0,0 +1,39 @@
+# Useless Docsify Tabs
+
+Simple docsify plugin for making tabs that work when embedded.
+
+## Installation
+
+```
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/useless-media/useless-docsify-tabs@master/src/css/useless-docsify-tabs.css">
+
+<script src="https://cdn.jsdelivr.net/gh/useless-media/useless-docsify-tabs@master/src/js/useless-docsify-tabs.js"></script>
+```
+
+## Usage
+
+```
+<div class="useless-tab-container">
+
+- Tab1
+
+  Tab1 content
+
+- Tab2
+
+  Tab2 content
+
+</div>
+```
+
+or
+
+```
+- Tab1 :useless-tab
+
+  Tab1 content
+
+- Tab2 :useless-tab
+
+  Tab2 content
+```
---
+++ src/js/useless-docsify-tabs.js
@@ -0,0 +1,72 @@
+(() => {
+  window.$docsify.plugins = [].concat((hook) => {
+    const includes = (str, value) => str.indexOf(value) > -1;
+    const sanitize = str => str.replace(' :useless-tab', '').trim();
+
+    const buildTab = (item, index, containerId, itemId, titleHTML, container, insertBefore) => {
+      const input = document.createElement('input');
+      input.id = itemId;
+      input.type = 'radio';
+      input.setAttribute('name', containerId);
+      input.classList.add('useless-tab');
+      input.innerHTML = titleHTML;
+      input.checked = index === 0;
+
+      const label = document.createElement('label');
+      label.setAttribute('for', itemId);
+      label.innerHTML = titleHTML;
+
+      container.insertBefore(input, insertBefore);
+      container.insertBefore(label, insertBefore);
+
+      item.classList.add('useless-tab-content');
+      item.removeChild(item.firstChild);
+    };
+
+    hook.doneEach(function () {
+      let containerIndex = -1;
+
+      document
+        .querySelectorAll('.useless-tab-container')
+        .forEach(container => {
+          const containerId = `useless-tab-container-${++containerIndex}`;
+          const items = container.querySelectorAll('li');
+
+          items.forEach((item, index) => {
+            const list = item.parentElement;
+            const titleHtml = sanitize(item.firstChild.innerHTML);
+            const titleText = sanitize(item.firstChild.innerText);
+            const itemId = `${containerId}-${titleText}`;
+
+            buildTab(item, index, containerId, itemId, titleHtml, container, list);
+          });
+        });
+
+      let itemIndex = -1;
+      let insertBefore;
+      document
+        .querySelectorAll('li')
+        .forEach((item) => {
+          if (item.parentElement.parentElement.classList.contains('useless-tab-container')) return;
+
+          const titleHtml = sanitize(item.firstChild.innerHTML);
+          const text = item.firstChild.innerText;
+          if (!includes(text, ':useless-tab')) return;
+
+          const list = item.parentElement;
+          const block = list;
+          block.classList.add('useless-tab-container');
+          let containerId = block.dataset.blockId;
+          if (!containerId) {
+            containerId = block.dataset.blockId = `useless-tab-container-${++containerIndex}`;
+            itemIndex = -1;
+            insertBefore = item;
+          }
+          const titleText = sanitize(text);
+          const itemId = `${containerId}-${titleText}`;
+
+          buildTab(item, ++itemIndex, containerId, itemId, titleHtml, block, insertBefore);
+        });
+    });
+  }, $docsify.plugins);
+})();
